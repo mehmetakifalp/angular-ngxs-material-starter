fd7028b9c4bd50ef2f5ee1822678cddfa4eea3f4
mehmetakifalp <mehmetakifalp@gmail.com>
2019-02-27 17:13:55 +0300

    order state

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,11 @@
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 import { UiModule } from '../ui/ui.module';
+import { OrderFormComponent } from './forms/order.form.component';
 
 @NgModule({
   imports: [UiModule],
   exports: [],
-  declarations: []
+  declarations: [OrderFormComponent]
 })
 export class PagesModule { }
---
+++ src/app/store/order.state.ts
@@ -0,0 +1,22 @@
+const defaults = {
+  orderForm: {
+    model : { name : '', count: 1},
+    dirty: false,
+    status: "",
+    errors: {}
+  },
+  orders: []
+};
+@State({
+  name: "OrderState",
+  defaults:defaults
+})
+export class OrderState{
+static defaults = defaults;
+
+constructor(private store: Store){
+
+}
+
+
+}
